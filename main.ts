786392e1676ed3d2dba45d5130da19ec356f397e
Azeezat25 <92942107+Azeezat25@users.noreply.github.com>
2021-11-09 12:16:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,22 +5,47 @@ input.onButtonPressed(Button.B, function () {
     basic.showString("Shake Me")
 })
 input.onGesture(Gesture.Shake, function () {
-    number = randint(0, 3)
+    number = randint(0, 5)
     if (number == 0) {
-        basic.showString("You belong in Hufflepuff!")
-        music.playMelody("B C5 B C5 B C5 B C5 ", 120)
+        for (let index = 0; index < 3; index++) {
+            basic.showString("You belong in Hufflepuff!")
+            music.playMelody("B C5 B C5 B C5 B C5 ", 120)
+            basic.showIcon(IconNames.Target)
+        }
     } else if (number == 1) {
-        basic.showString("You belong in Ravenclaw!")
-        music.playMelody("G A G A G A G A ", 120)
+        for (let index = 0; index < 3; index++) {
+            basic.showString("You belong in Ravenclaw!")
+            music.playMelody("G A G A G A G A ", 120)
+            basic.showIcon(IconNames.Pitchfork)
+        }
     } else if (number == 2) {
-        basic.showString("You belong in Slytherin!")
-        music.playMelody("E F E F E F E F ", 120)
+        for (let index = 0; index < 3; index++) {
+            basic.showString("You belong in Slytherin!")
+            music.playMelody("E F E F E F E F ", 120)
+            basic.showIcon(IconNames.Triangle)
+        }
+    } else if (number == 3) {
+        for (let index = 0; index < 3; index++) {
+            basic.showString("You belong in Gryffindor!")
+            music.playMelody("C D C D C D C D ", 120)
+            basic.showIcon(IconNames.LeftTriangle)
+        }
+    } else if (number == 4) {
+        for (let index = 0; index < 3; index++) {
+            basic.showString("Special class-NONE!")
+            music.playMelody("E D C C C C C C ", 120)
+            basic.showIcon(IconNames.Skull)
+        }
     } else {
-        basic.showString("You belong in Gryffindor!")
-        music.playMelody("C D C D C D C D ", 120)
+        for (let index = 0; index < 3; index++) {
+            basic.showString("Special class-ALL!")
+            music.playMelody("A B C5 C5 C5 C5 C5 C5 ", 120)
+            basic.showIcon(IconNames.Ghost)
+        }
     }
 })
 let number = 0
+basic.showString("Hello!")
 basic.showString("Shake Me")
 basic.forever(function () {
 	
